abd78be31dd167e9c6e4f2018406f18024191257
mantana04 <ja_kitty@hotmail.com>
2016-10-17 14:33:38 +0700

    basic1 project

---
+++ array1.php
@@ -0,0 +1,19 @@
+<?php
+//$car = array("toyota","honda","isuzu");
+$car = ["toyota","honda","isuzu"];
+        //print_r($car);
+        //echo '<hr>';
+$count_car= count($car);
+        //echo $car[1];
+        
+for ($x = 0; $x < $count_car; $x++){
+    echo $car[$x].'<br>';
+}
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ oop1.php
@@ -0,0 +1,28 @@
+<?php
+
+class car{
+    public $color; //property
+    
+    public function setColor($c){
+        return 'Car is : '.$this->color=$c;
+   }  
+   public function sayHello(){
+       return 'say Hello';
+   }
+}
+$newcar=new car;
+$test=$newcar->setColor('red');
+echo $test;
+echo '<hr>';
+
+echo $newcar->sayHello();
+echo '<hr>';
+echo $newcar->color='orange';
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
